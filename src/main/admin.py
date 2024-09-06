1bc8f6d1addcef0debff226aaef18764f16e31be
Tuych <tuychiyevbaxtiyor06@gmail.com>
2024-09-06 15:47:25 +0500

    created 3 models

--- src/main/admin.py
+++ src/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . models import AboutMe, Skills, Projects
 
-# Register your models here.
+
+admin.site.register(AboutMe)
+admin.site.register(Skills)
+admin.site.register(Projects)
--- src/main/models.py
+++ src/main/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class AboutMe(models.Model):
+    first_name = models.CharField(max_length=30, blank=False, null=False)
+    last_name = models.CharField(max_length=30, blank=False, null=False)
+    birthday = models.DateField(blank=False, null=False)
+    city = models.CharField(max_length=50, null=True, blank=True)
+    degree = models.CharField(max_length=50, blank=True, null=True)    # degree daraja
+    phone_number = models.CharField(max_length=13, null=True, blank=True)
+    email = models.EmailField(max_length=50, null=True, blank=True, unique=True)
+    git_hub_url = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.first_name
+
+
+class Skills(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    level_of_knowledge = models.SmallIntegerField(default=0,)        #  bilish darajasi skillarni
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Projects(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    category = models.CharField(max_length=30, blank=False, null=False)
+    description = models.TextField(null=True, blank=True)
+    client = models.CharField(max_length=50, default='Portfolio project')
+    project_date = models.DateField(blank=True, null=True)
+    project_url = models.URLField(blank=False, null=False)
+    image_title = models.ImageField(upload_to='portfolio/', default='portfolio/title-default.png')
+    image_main = models.ImageField(upload_to='portfolio/', default='portfolio/main-default.png')
+
+    def __str__(self):
+        return self.name
